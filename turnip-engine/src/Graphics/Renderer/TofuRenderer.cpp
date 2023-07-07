29dbf1465bc8d00bbe271e188118e84f098c183f
DatDarkAlpaca <paulocastrogalvao@hotmail.com>
2023-07-06 20:23:23 -0400

    Adds a draw quad function to the renderer

--- turnip-engine/src/Graphics/Renderer/TofuRenderer.cpp
+++ turnip-engine/src/Graphics/Renderer/TofuRenderer.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "TofuRenderer.h"
 
+#include "Graphics/Vertex.h"
+
 namespace tur
 {
 	void TofuRenderer::Initialize(const TofuRendererProps& properties)
@@ -30,6 +32,40 @@ namespace tur
 		shader.Bind();
 
 		glDrawElements(GL_TRIANGLES, mesh.GetElementCount(), GL_UNSIGNED_INT, nullptr);
+	}
+
+	void TofuRenderer::DrawQuad(const Transform& transform, Texture& texture, Shader& shader)
+	{
+		Mesh mesh;
+		float width = 100 / 800.f, height = 100 / 600.f;
+		std::vector<Vertex> vertices
+		{
+			{ {  width, height, 0.0f }, { 0.0f , 0.0f , 0.0f }, { 1.0f, 1.0f, 0.5f, 1.0f }, { 1.0f, 1.0f } },
+			{ { -width,  height, 0.0f }, { 0.0f , 0.0f , 0.0f }, { 0.4f, 1.0f, 0.2f, 1.0f }, { 0.0f, 1.0f } },
+			{ { -width, -height, 0.0f }, { 0.0f , 0.0f , 0.0f }, { 0.5f, 1.0f, 0.5f, 1.0f }, { 0.0f, 0.0f } },
+			{ {  width, -height, 0.0f }, { 0.0f , 0.0f , 0.0f }, { 1.0f, 0.6f, 0.3f, 1.0f }, { 1.0f, 0.0f } }
+		};
+		std::vector<U32> indices { 0, 1, 2, 2, 3, 0 };
+
+		mesh.Initialize(vertices, indices);
+
+		texture.Bind();
+		mesh.Bind();
+		shader.Bind();
+
+		glm::mat4 model(1.f);
+		glm::mat4 rotation = glm::toMat4(glm::quat(transform.rotation));
+
+		model = glm::translate(model, transform.position);
+		model = glm::scale(model, transform.scale);
+		model *= rotation;
+
+		shader.SetMatrix4f("u_model", model);
+
+		glDrawElements(GL_TRIANGLES, mesh.GetElementCount(), GL_UNSIGNED_INT, nullptr);
+
+		mesh.Unbind();
+		mesh.Destroy();
 	}
 
 	void TofuRenderer::End()
