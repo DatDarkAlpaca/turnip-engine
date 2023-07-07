4712ff9386290f2da19ea14d3f4447d2ecfb2e47
DatDarkAlpaca <paulocastrogalvao@hotmail.com>
2023-07-06 20:35:20 -0400

    Further cleans up the editor

--- turnip-editor/src/EditorView.cpp
+++ turnip-editor/src/EditorView.cpp
@@ -5,56 +5,39 @@ struct Component { int a; };
 void EditorView::OnInitialize()
 {
     m_Shader = new Shader({
-         { "res/basic.vert", ShaderType::TUR_SHADER_VERTEX },
-         { "res/basic.frag", ShaderType::TUR_SHADER_FRAGMENT },
-        });
+        { "res/basic.vert", ShaderType::TUR_SHADER_VERTEX },
+        { "res/basic.frag", ShaderType::TUR_SHADER_FRAGMENT },
+    });
 
     m_Texture = std::make_shared<Texture>();
     m_Texture->Initialize("res/turnip.png");
 
-    m_Texture1 = std::make_shared<Texture>();
-    m_Texture->Initialize("res/turnip1.png");
-
-    Entity entity = m_CurrentScene.NewEntity();
-    entity.AddComponent<ComponentTransform>();
-    entity.AddComponent<ComponentTexture>(m_Texture);
-
-    Entity entity1 = m_CurrentScene.NewEntity();
-    entity1.AddComponent<ComponentTransform>();
-    entity1.AddComponent<ComponentTexture>(m_Texture1);
+    {
+        Entity entity = m_CurrentScene.NewEntity();
+        entity.AddComponent<ComponentTransform>();
+        entity.AddComponent<ComponentTexture>(m_Texture);
+    }
 
     TofuRenderer::SetColor("#FAF4E8FF");
 }
 
 void EditorView::OnUpdate()
 {
-    TofuRenderer::SetColor(m_Color);
     TofuRenderer::Begin();
 
-    if (m_Show)
+    auto view = m_CurrentScene.GetRegistry()->view<ComponentTransform, ComponentTexture>();
+    for (auto&& [entity, transform, texture] : view.each())
     {
-        auto view = m_CurrentScene.GetRegistry()->view<ComponentTransform, ComponentTexture>();
-        for (auto&& [entity, transform, texture] : view.each())
-        {
-            auto useTranform = transform.transform;
-            auto useTexture = texture.texture.lock();
-            TofuRenderer::DrawQuad(useTranform, *useTexture.get(), *m_Shader);
-        }
+        auto useTranform = transform.transform;
+        auto useTexture = texture.texture.lock();
+        TofuRenderer::DrawQuad(useTranform, *useTexture.get(), *m_Shader);
     }
-    
 
     TofuRenderer::End();
 }
 
 void EditorView::OnRenderGUI()
 {
-    ImGui::Begin("Color Picker");
-    ImGui::ColorPicker4("Color Picker", &m_Color[0]);
-    ImGui::End();
-
-    ImGui::Begin("Position");
-    ImGui::SliderFloat3("Position", &m_Pos[0], -1, 1);
-    ImGui::End();
 }
 
 void EditorView::OnShutdown()
--- turnip-editor/src/EditorView.h
+++ turnip-editor/src/EditorView.h
@@ -20,7 +20,4 @@ private:
 private:
     std::shared_ptr<Texture> m_Texture;
     Shader* m_Shader = nullptr;
-
-    glm::vec3 m_Pos = glm::vec3(0.f);
-    glm::vec4 m_Color = glm::vec4(0.f, 0.f, 0.f, 1.f);
 };
