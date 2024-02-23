cebf1b4096d14cc70cf340703ba5241cab3880ce
Alpaca <paulocastrogalvao@hotmail.com>
2024-02-22 23:16:54 -0400

    Update state

--- turnip-engine/src/Util/State/State.h
+++ turnip-engine/src/Util/State/State.h
@@ -1,59 +1,74 @@
 #pragma once
 #include <bitset>
+#include <numeric>
 #include <type_traits>
 
-namespace tur
+#define DECLARE_STATE(StateName)						\
+template<>												\
+struct StateTraits<StateName>							\
+{														\
+	static const StateName max = StateName::MAX_ITEMS;  \
+};
+
+template<typename T>
+struct StateTraits;
+
+template<typename T>
+class State
 {
-	template<typename T>
-	struct StateTraits;
+	using StateType = typename std::underlying_type<T>::type;
 
-	template<typename T>
-	class State
+public:
+	explicit State() = default;
+
+	State(const std::bitset<static_cast<StateType>(StateTraits<T>::max)>& bitset)
+		: m_Bitset(bitset)
 	{
-		using StateType = typename std::underlying_type<T>::type;
 
-	public:
-		bool Test(T pos) const
-		{
-			return m_Bitset.test(Value(pos));
-		}
+	}
 
-		State& Reset(T pos)
-		{
-			m_Bitset.reset(Value(pos));
-			return *this;
-		}
+public:
+	State& Set(T position, bool value = true)
+	{
+		m_Bitset.set(Bit(position), value);
+		return *this;
+	}
 
-		State& Flip(T pos)
-		{
-			m_Bitset.flip(Value(pos));
-			return *this;
-		}
+public:
+	auto operator[] (T position)
+	{
+		return m_Bitset[Bit(position)];
+	}
 
-	public:
-		State operator| (State lhs)
-		{
-			State state;
-			state.m_Bitset = m_Bitset | lhs.m_Bitset;
+	auto operator[] (uint64_t position)
+	{
+		return m_Bitset[position];
+	}
 
-			return state;
-		}
+	State operator| (const State& lhs)
+	{
+		return Bitset() | lhs.Bitset();
+	}
 
-		State operator& (State lhs)
-		{
-			State state;
-			state.m_Bitset = m_Bitset & lhs.m_Bitset;
+	State operator& (const State& lhs)
+	{
+		return Bitset() & lhs.Bitset();
+	}
 
-			return state;
-		}
+public:
+	auto Bitset() const { return m_Bitset; }
 
-	private:
-		StateType Value(T v) const
-		{
-			return static_cast<StateType>(v);
-		}
+private:
+	inline StateType Value(T value) const
+	{
+		return static_cast<StateType>(value);
+	}
 
-	public:
-		std::bitset<static_cast<StateType>(StateTraits<T>::max)> m_Bitset;
-	};
-}
+	inline uint64_t Bit(T value) const
+	{
+		return static_cast<uint64_t>(value) >> 1;
+	}
+
+private:
+	std::bitset<static_cast<StateType>(StateTraits<T>::max)> m_Bitset;
+};
