8cbbc944db7d8fcb28e9895a2f5b1ea785cfa26c
DatDarkAlpaca <paulovicg@outlook.com>
2025-04-24 12:34:47 +0900

    Component adding logic

--- turnip-editor/src/widget/entity_inspector.hpp
+++ turnip-editor/src/widget/entity_inspector.hpp
@@ -17,34 +17,66 @@ public:
 	{
 		ImGui::Begin("Entity Inspector");
 
+		if (m_SceneData->viewerSelectedEntity == entt::null)
+		{
+			ImGui::End();
+			return;
+		}
+		
 		Entity entity(m_SceneData->viewerSelectedEntity, m_Scene);
 
-		if (entity.has_component<TransformComponent>())
-		{
-			TransformComponent& transform = entity.get_component<TransformComponent>();
+		render_components(entity);
 
-			ImGui::BeginGroup();
-
-				ImGui::Text("Position"); 
-				ImGui::DragFloat3("##position", &transform.transform.position[0]);
-
-				ImGui::Text("Rotation"); 
-				ImGui::DragFloat3("##rotation", &transform.transform.rotation[0], 0.5f, 0.0f, 360.f);
-			
-				ImGui::Text("Scale"); 
-				ImGui::DragFloat3("##scale", &transform.transform.scale[0]);
-
-			ImGui::EndGroup();
-		}
+		ImGui::Separator();
+		
+		render_component_list(entity);
 
 		ImGui::End();
 	}
 
 private:
+	void render_components(Entity selectedEntity)
+	{
+		if (selectedEntity.has_component<TransformComponent>())
+			render_transform_component(selectedEntity);
+	}
+
+	void render_transform_component(Entity selectedEntity)
+	{
+		auto& transform = selectedEntity.get_component<TransformComponent>().transform;
+
+		if (ImGui::CollapsingHeader("Transform"))
+		{
+			ImGui::Text("Position");
+			ImGui::DragFloat3("##position", &transform.position[0]);
+
+			ImGui::Text("Rotation");
+			ImGui::DragFloat3("##rotation", &transform.rotation[0], 0.5f, 0.0f, 360.f);
+
+			ImGui::Text("Scale");
+			ImGui::DragFloat3("##scale", &transform.scale[0]);
+		}		
+	}
+
+private:
+	void render_component_list(Entity selectedEntity)
+	{
+		if (ImGui::Button("Add Component"))
+			ImGui::OpenPopup("AddComponentPopup");
+
+		if (ImGui::BeginPopup("AddComponentPopup")) 
+		{
+			if (!selectedEntity.has_component<TransformComponent>() && ImGui::MenuItem("Transform"))
+			{
+				Transform transform(glm::mat4(1.0f));
+				selectedEntity.add_component<Transform>(transform);
+			}
+			
+			ImGui::EndPopup();
+		}
+	}
+
+private:
 	NON_OWNING Scene* m_Scene = nullptr;
 	SceneData* m_SceneData = nullptr;
-
-	entt::entity m_SelectedEntity;
-	entt::entity m_RenamingEntity;
-	std::string m_RenameString;
 };
